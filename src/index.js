40fbde8f7a5f36fb5383c18fc73e7fc0d1969a24
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-02-11 13:09:51 -0800

    created booc card

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import * as Config_private from "./config_private.js";
+import * as config from "./config.js";
 
 /********************************************************* */
 /**DOM elements */
 const quoteBlock = document.getElementById("quoteBlock")
 const searchBtn = document.getElementById("search-btn")
+const bookCards = document.getElementById("searchResult");
 
 // Set config defaults when creating the instance_quote
 const instance_quote = axios.create();
@@ -17,8 +19,7 @@ instance_quote.defaults.headers.common['x-api-key'] = Config_private.API_KEY_Q;
 
 (
 function getQutation() {
-    const url = "https://api.api-ninjas.com/v1/quotes?category=dreams"
-        instance_quote.get(url)
+        instance_quote.get(config.quote_url)
         .then((response)=>{
             quoteBlock.querySelector("#quote").appendChild(document.createElement("h3")).textContent = response.data[0].quote;
             quoteBlock.querySelector("#author").appendChild(document.createElement("h6")).textContent = response.data[0].author;
@@ -33,10 +34,14 @@ searchBtn.addEventListener('click', searchBooks);
 function searchBooks(event){
     event.preventDefault();
     const q = document.getElementById("search-req").value.trim().replaceAll(" ","+");
-    console.log(q)
-    const url = `https://openlibrary.org/search.json?q=${q}`
+    const url = `${config.search_lib_url}?q=${q}&fields=${config.search_filter}`
     instance_lib.get(url)
     .then((response)=>{
         console.log(response.data)
+        const cardTemplate = document.getElementById("bookCard");
+        const cardClone = cardTemplate.content.cloneNode(true);
+        cardClone.querySelector(".card-title").textContent = response.data.docs[0].title
+        cardClone.querySelector(".card-subtitle").textContent = response.data.docs[0].author_name[0].toString();
+bookCards.append(cardClone);
     })
 }
