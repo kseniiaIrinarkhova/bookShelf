67078c1449a785a8f8732c3530e5cac2a825ad9b
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-02-11 21:07:37 -0800

    fixed event handler on main page

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import * as Utilities from "./utilities.js";
 /**DOM elements */
 const quoteBlock = document.getElementById("quoteBlock")
 const searchBtn = document.getElementById("search-btn")
-const bookCards = document.getElementById("searchResult");
 const navLinks = document.querySelector("nav");
 
 // Set config defaults when creating the instance_quote
@@ -17,75 +16,123 @@ const instance_lib = axios.create();
 // Alter defaults after instance_quote has been created
 instance_quote.defaults.headers.common['x-api-key'] = Config_private.API_KEY_Q;
 
+instance_lib.defaults.headers.post['Content-Type'] = 'application/json';
+
 
 (
-function getQutation() {
+    function getQutation() {
         instance_quote.get(config.quote_url)
-        .then((response)=>{
-            quoteBlock.querySelector("#quote").appendChild(document.createElement("h3")).textContent = response.data[0].quote;
-            quoteBlock.querySelector("#author").appendChild(document.createElement("h6")).textContent = response.data[0].author;
-        })
-        .catch((error)=>{console.log(error);});
-}
+            .then((response) => {
+                quoteBlock.querySelector("#quote").appendChild(document.createElement("h3")).textContent = response.data[0].quote;
+                quoteBlock.querySelector("#author").appendChild(document.createElement("h6")).textContent = response.data[0].author;
+            })
+            .catch((error) => { console.log(error); });
+    }
 )();
 
 searchBtn.addEventListener('click', searchBooks);
 
-function searchBooks(event){
+function searchBooks(event) {
     event.preventDefault();
+    const bookCards = document.getElementById("searchResult");
     Utilities.clearElement(bookCards);
-    const q = document.getElementById("search-req").value.trim().replaceAll(" ","+");
-    if(String(q).length){
+    const q = document.getElementById("search-req").value.trim().replaceAll(" ", "+");
+    if (String(q).length) {
         const url = `${config.lib_url}/search.json?q=${q}&fields=${config.search_filter}&limit=3`
-    instance_lib.get(url)
-    .then((response)=>{
-        Utilities.createCards(response.data.docs, bookCards);
-    })
-}
+        instance_lib.get(url)
+            .then((response) => {
+                Utilities.createCards(response.data.docs, bookCards);
+            })
+    }
 }
 
-navLinks.addEventListener("click",(event)=>{
-event.preventDefault();
+navLinks.addEventListener("click", (event) => {
+    event.preventDefault();
     const contentBlock = document.getElementById("contentBlock");
-switch (event.target.id) {
-    case "myList":
-        Utilities.changeMainblock(contentBlock, "myList-tmp")
-        .then((res)=>{
-            getMyList();
-        })
-        break;
-    case "home":
-        Utilities.changeMainblock(contentBlock, "main-tmp");
-        break;
-    default:
-        break;
-}
+    switch (event.target.id) {
+        case "myList":
+            Utilities.changeMainblock(contentBlock, "myList-tmp")
+                .then((res) => {
+                    getMyList();
+                })
+            break;
+        case "home":
+            Utilities.changeMainblock(contentBlock, "main-tmp")
+                .then((res) => {
+                    console.log(res)
+                    document.getElementById("search-btn").addEventListener('click', searchBooks);
+                })
+            
+            break;
+        default:
+            break;
+    }
 });
 
 
-function getMyList(){
+function getMyList() {
     instance_lib.get(`${config.lib_url}/people/${config.libAPIUser}/lists.json`)
-    .then((res)=>{
+        .then((res) => {
 
-        console.log(res.data.entries[0])
-        instance_lib.get(`${config.lib_url}${res.data.entries[0].url}/seeds.json`)
-        .then((resp)=>{
-            console.log(resp.data)
-            
-            Utilities.createCards(getBooksInfo(resp.data.entries), document.getElementById("listBody"));
+            console.log(res.data.entries[0])
+            instance_lib.get(`${config.lib_url}${res.data.entries[0].url}/seeds.json`)
+                .then((resp) => {
+                    console.log(resp.data)
+
+                    Utilities.createCards(getBooksInfo(resp.data.entries), document.getElementById("listBody"));
+                })
         })
-    })
 }
 
 function getBooksInfo(listEntries) {
-    let booksArray=[];
-    Array.prototype.forEach.call(listEntries,(entry)=>{
+    let booksArray = [];
+    Array.prototype.forEach.call(listEntries, (entry) => {
         booksArray.push({
             "title": entry.title,
-            "author_name":[""],
+            "author_name": [""],
             "picture_url": entry.picture.url,
-            "cover_i": "14444481"
         })
     })
-return booksArray;
+    return booksArray;
 }
+
+bookCards.addEventListener("click", cardClick);
+
+async function cardClick(event) {
+    if (event.target.classList.contains("listAdd")) {
+        event.preventDefault()
+
+        const requestBody = {
+            add: [
+                { key: Utilities.getKey(event.target) }
+            ]
+        }
+        instance_lib.get(`${config.lib_url}/people/${config.libAPIUser}/lists.json`)
+            .then((res) => {
+                const url = `${config.lib_url}${res.data.entries[0].url}/seeds.json`
+                console.log("before fetch")
+                console.log(url)
+                fetch(url,
+                    {
+                        method: "POST",
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Accept': 'application/json'
+                        },
+                        body: JSON.stringify(requestBody),
+                    })
+                    .then((res) => {
+                        console.log(res.json())
+                    })
+                    .catch((err) => { console.log(err); throw err });
+            })
+            .catch((err) => {
+                alert("Cannot add book to list due to CORS error")
+                throw err;
+            })
+            .catch((err) => { console.log(err) });
+
+        console.log(`${config.lib_url}/people/${config.libAPIUser}/seeds`)
+    }
+}
+
--- src/utilities.js
+++ src/utilities.js
@@ -8,10 +8,19 @@ function createCards(books, parentElement) {
     const cardTemplate = document.getElementById("bookCard");
     Array.prototype.forEach.call(books, (book) => {
         const cardClone = cardTemplate.content.cloneNode(true);
+        console.log(book)
+        if("cover_i" in book){
         cardClone.querySelector('img').setAttribute('src', `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`);
         cardClone.querySelector('img').setAttribute('alt', `${book.title} ${book.author_name[0].toString()}`);
+        }
+        else{
+            const url = `https:${book.picture_url}`.replace('-S','-M')
+            cardClone.querySelector('img').setAttribute('src', url);
+            cardClone.querySelector('img').setAttribute('alt', `${book.title}`);
+        }
         cardClone.querySelector(".card-title").textContent = book.title
         cardClone.querySelector(".card-subtitle").textContent = book.author_name[0].toString();
+        cardClone.querySelector(".key").textContent = book.key
         parentElement.append(cardClone);
     });
 }
@@ -22,4 +31,10 @@ async function changeMainblock(parentElement, templateName){
     const clone = template.content.cloneNode(true);
     parentElement.append(clone);
 }
-export {clearElement, createCards,changeMainblock}
+
+function getKey(element) {
+    console.log(element.parentNode.parentNode)
+    const card = element.parentNode.parentNode;
+    return card.querySelector(".key").textContent
+}
+export {clearElement, createCards,changeMainblock,getKey}
